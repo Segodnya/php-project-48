ebc64c6b1c4e1492ca1b9d85af1a99839ea99035
kkurentsov <kkurentsov@team.amocrm.com>
2024-09-15 12:44:52 +0300

    [refactor] reduce code duplication in tests

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -3,10 +3,8 @@
 namespace Hexlet\Code\Tests;
 
 use PHPUnit\Framework\TestCase;
-use Symfony\Component\Yaml\Yaml;
 
-use function Hexlet\Code\Differ\diff;
-use function Hexlet\Code\Formatters\format;
+use function Differ\Differ\genDiff;
 
 class DifferTest extends TestCase
 {
@@ -16,20 +14,8 @@ class DifferTest extends TestCase
     public function testGenDiff(string $file1, string $file2, string $format, string $expectedFile): void
     {
         $expected = file_get_contents(__DIR__ . "/Fixtures/{$expectedFile}");
-        $file1Content = file_get_contents(__DIR__ . "/Fixtures/{$file1}");
-        $file2Content = file_get_contents(__DIR__ . "/Fixtures/{$file2}");
-
         $this->assertNotFalse($expected);
-        $this->assertNotFalse($file1Content);
-        $this->assertNotFalse($file2Content);
-
-        $data1 = $this->parseFile($file1, $file1Content);
-        $data2 = $this->parseFile($file2, $file2Content);
-
-        $this->assertIsArray($data1);
-        $this->assertIsArray($data2);
-
-        $actual = format(diff($data1, $data2), $format);
+        $actual = genDiff(__DIR__ . "/Fixtures/{$file1}", __DIR__ . "/Fixtures/{$file2}", $format);
         $this->assertEquals($expected, $actual);
     }
 
@@ -44,18 +30,5 @@ class DifferTest extends TestCase
             'JSON and JSON to Plain' => ['file1.json', 'file2.json', 'plain', 'diff.plain'],
             'YAML and YAML to Stylish' => ['file1.yaml', 'file2.yaml', 'stylish', 'diff.stylish'],
         ];
-    }
-
-    /**
-     * @return array<mixed>
-     */
-    private function parseFile(string $filename, string $content): array
-    {
-        $extension = pathinfo($filename, PATHINFO_EXTENSION);
-        $parsedContent = $extension === 'json'
-            ? json_decode($content, true)
-            : Yaml::parse($content);
-
-        return is_array($parsedContent) ? $parsedContent : [];
     }
 }
